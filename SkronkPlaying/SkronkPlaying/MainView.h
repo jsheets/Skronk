aaf9e51dfe91a2d88ad4a73f87b40caf1780e6e1
John Sheets <jsheets@metacasa.net>
2011-10-02 22:58:45 -0500

    Custom top-level view MainView.

---
+++ SkronkPlaying/SkronkPlaying/MainView.h
@@ -0,0 +1,13 @@
+//
+//  MainView.h
+//  SkronkPlaying
+//
+//  Created by John Sheets on 10/2/11.
+//  Copyright (c) 2011 JAMF Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MainView : NSView
+
+@end
---
+++ SkronkPlaying/SkronkPlaying/MainView.m
@@ -0,0 +1,23 @@
+//
+//  MainView.m
+//  SkronkPlaying
+//
+//  Created by John Sheets on 10/2/11.
+//  Copyright (c) 2011 JAMF Software. All rights reserved.
+//
+
+#import "MainView.h"
+
+@implementation MainView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    if ((self = [super initWithFrame:frame]))
+    {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
