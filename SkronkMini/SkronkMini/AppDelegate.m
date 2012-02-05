1d100dc2e5fa5aa571aba730721d1408015d512a
John Sheets <jsheets@metacasa.net>
2012-02-05 15:37:49 -0600

    One-line window with track label. Float to top.

--- SkronkMini/SkronkMini/AppDelegate.m
+++ SkronkMini/SkronkMini/AppDelegate.m
@@ -11,10 +11,23 @@
 @implementation AppDelegate
 
 @synthesize window = _window;
+@synthesize label = _label;
+
+- (void)updateCurrentTrackForUser:(NSString *)username
+{
+    NSString *urlString = [NSString stringWithFormat:@"http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&api_key=3a36e88356d8d90aee7a012c6abccae1&limit=2&user=%@&format=json", username];
+    NSURL *url = [NSURL URLWithString:urlString];
+
+    NSLog(@"Looking up last.fm URL: %@", url);
+}
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+    self.label.stringValue = @"Artist - Album - Track";
+    [self.window setMovableByWindowBackground:YES];
+    self.window.level = NSFloatingWindowLevel;
+
+    [self updateCurrentTrackForUser:@"johnsheets"];
 }
 
 @end
