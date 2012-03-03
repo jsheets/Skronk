e4c0dfd9a02a2d3874e1b64ead021e44361862ae
John Sheets <jsheets@metacasa.net>
2012-03-03 12:40:23 -0600

    Add subtle inset shading.

--- SkronkMini/SkronkMini/RoundedView.m
+++ SkronkMini/SkronkMini/RoundedView.m
@@ -24,21 +24,30 @@
 
 - (void)drawRect:(NSRect)dirtyRect
 {
-    // Drawing code here.
     [[NSColor windowBackgroundColor] set];
 
     [NSGraphicsContext saveGraphicsState];
 
     // Clip view to rounded corners.
     NSBezierPath *path = [NSBezierPath bezierPathWithRoundedRect:self.bounds xRadius:10 yRadius:10];
-    path.lineWidth = 3.0;
     [path addClip];
+    path.lineWidth = 2.0;
 
     // Concrete background.
     [self.backgroundImage compositeToPoint:NSZeroPoint fromRect:self.bounds operation:NSCompositeSourceOver];
 
-    // Gray border.
-    [[NSColor grayColor] set];
+    NSRect insetRect = NSInsetRect(self.bounds, 4, 4);
+    NSBezierPath *insetPath = [NSBezierPath bezierPathWithRoundedRect:insetRect xRadius:8 yRadius:8];
+
+    // Slightly darker text background.
+    [[NSColor colorWithDeviceWhite:0.0 alpha:0.15] set];
+    [insetPath fill];
+
+    // Border around text background.
+    [[NSColor colorWithDeviceWhite:0.0 alpha:0.4] set];
+    [insetPath stroke];
+
+    // Outer border.
     [path stroke];
 
     [NSGraphicsContext restoreGraphicsState];
